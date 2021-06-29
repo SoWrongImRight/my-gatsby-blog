865c9329b64d92bf25fbc4b694b3e715647a22a3
Russ Carroll <russ.carroll@pm.me>
2021-06-29 13:00:20 -0400

    Corrected issues (files were set to UTF-16 somehow) and added light/dark mode toggle to header.

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import { Link as GatsbyLink } from "gatsby";
 import React from "react";
-import { Box, Button, Heading, Link, useColorMode } from "theme-ui";
+import { Box, Button, Flex, Heading, Link, useColorMode } from "theme-ui";
 
 export const Header = ({ siteTitle, siteDescription }) => {
     const [ colorMode, setColorMode ] = useColorMode();
@@ -14,19 +14,23 @@ export const Header = ({ siteTitle, siteDescription }) => {
                     p: '1.45rem 1.0875rem',
                 }}
             >
-                <Link as={GatsbyLink} to="/">
-                    <Heading>{siteTitle}</Heading>
-                </Link>
-                <Box as='p' variant='styles.p'>
-                    {siteDescription}
-                </Box>
-                <Button
-                    onClick={(e) => {
-                        setColorMode(colorMode === 'default' ? 'dark' : 'default');
-                    }}
-                >
-                    {colorMode == 'default' ? 'Dark' : 'Light'}
-                </Button>
+                <Flex>
+                    <Box sx={{ flex: '1 1 auto', flexDirection: 'column'}}>
+                        <Link as={GatsbyLink} to="/">
+                            <Heading>{siteTitle}</Heading>
+                        </Link>
+                        <Box as='p' variant='styles.p'>
+                            {siteDescription}
+                        </Box>
+                    </Box>
+                    <Button
+                        onClick={(e) => {
+                            setColorMode(colorMode === 'default' ? 'dark' : 'default');
+                        }}
+                    >
+                        {colorMode == 'default' ? 'Dark' : 'Light'}
+                    </Button>
+                </Flex>
             </Box>
         </Box>
     );
